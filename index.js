07e37d85e9fe928e9fae0001ba164facbd50b4bc
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-09-05 20:56:19 +0530

    Entry point edit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const inputValidator = require('./lib/validate-input/ValidateInput')
+const validateForm = require('./lib/validate-form/validateForm')
 
 exports.validateInput = inputValidator;
+exports.validateForm = validateForm;
