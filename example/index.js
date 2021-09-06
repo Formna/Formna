9165498e76ee32d3643f53ec5318118a70246541
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-09-06 10:21:42 +0530

    implement complete form validation example

--- example/index.js
+++ example/index.js
@@ -1,3 +1,34 @@
-import { validateInput } from 'formna'
+import { validateInput, validateForm } from "formna";
 
-const nameInput = validateInput("name", "nameError", "", "Enter a valid name", "error", (inputValue) => inputValue.trim().length > 6)
+//Single input validation
+const nameInput = validateInput(
+  "name",
+  "nameError",
+  "",
+  "Enter a valid name",
+  "error",
+  (inputValue) => inputValue.trim().length > 6
+);
+
+// Complete form validation
+const className = "form";
+const fileldDetails = [
+  {
+    id: "email", //id of the input field
+    errorId: "emailError", //id of the error field of the respective input
+    initialValue: "", // initial value of the input
+    errorMessage: "Enter a valid email", // error message to be shown
+    errorClassName: "error", // class related to changes need to be seen in the input element
+    validator: (inputVal) => inputVal.includes("@"), // validation logic
+  },
+  {
+    id: "password",
+    errorId: "passwordError",
+    initialValue: "",
+    errorMessage: "Enter a valid password",
+    errorClassName: "error",
+    validator: (inputVal) => inputVal.toString().trim().length > 6,
+  },
+];
+const postEndPoint = ""; // this is completely optional. you can see the same results without passing the third parameter.
+validateForm(className, fileldDetails, postEndPoint);
